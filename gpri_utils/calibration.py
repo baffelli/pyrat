7ba4f88bca8f935c1940c198f79faa0c7e415ecc
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-03-23 15:45:15 +0000

    Final version of eusar paper (fixed typos) Improved colormap for phase
    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Code/pyrat@2245 7821f091-807e-4826-bf48-bc026ed4e502

--- gpri_utils/calibration.py
+++ gpri_utils/calibration.py
@@ -264,26 +264,6 @@ def patch_coregistration(im1, im2, n_patch, oversampling = (5,5)):
     return sh_list
 
 
-
-# def rep_2(r_arm, r_ph, r_sl, theta, wrap = True):
-#     """
-#     This function computes the phase caused by a shifted
-#     phase center in the antenna
-#     """
-#     lam = (3e8) /17.2e9
-#     ant_angle = _np.arctan2(r_ph, r_arm)
-#     r_ant = _np.sqrt(r_arm**2 + r_ph**2)
-#     # #angle beta
-#     #Chord length
-#     c = 2 * r_ant * _np.sin(theta/2)
-#     mixed_term = 2 * c * r_sl * _np.cos(_np.pi/2 - ant_angle - theta/2)
-#     dist = _np.sqrt(c**2 + r_sl**2 - mixed_term)
-#
-#     if wrap is True :
-#         return _np.mod(-4 * _np.pi * dist/lam, 2 * _np.pi), dist
-#     else:
-#         return (-4 * _np.pi * dist/lam), dist
-
 def distance_from_phase_center(r_arm, r_ph, r_sl, theta, wrap=False):
     """
     This function computes the phase caused by a shifted
@@ -291,10 +271,10 @@ def distance_from_phase_center(r_arm, r_ph, r_sl, theta, wrap=False):
     """
     lam = gpf.gpri_files.C / 17.2e9
     r_ant = _np.sqrt(r_arm**2 + r_ph**2)
-    ant_angle = _np.arctan2(r_ph,r_ant)
+    alpha = _np.arctan(r_ph / r_arm)
     #Chord length
     c = r_ant + r_sl
-    mixed_term = 2 * c * r_ant * _np.cos(theta - ant_angle)
+    mixed_term = 2 * c * r_ant * _np.cos(theta + alpha)
     dist = _np.sqrt(c**2 + r_ant**2 - mixed_term)
     if wrap is True :
         return _np.mod(-4 * _np.pi * dist/lam, 2 * _np.pi), dist
