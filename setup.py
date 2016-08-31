c927a39a972e080283636a5fbf36d703bc7564c1
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-08-31 12:40:21 +0000

    added setup to the pyrat package
    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Code/pyrat@2434 7821f091-807e-4826-bf48-bc026ed4e502

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Pyrat',
+      version='0.1',
+      description='Python radar tools',
+      author='Simone Baffelli',
+      author_email='baffelli@ifu.baug.ethz.ch',
+      license='MIT',
+      packages=['pyrat'],
+      zip_safe=False)
