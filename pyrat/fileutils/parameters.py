fe2d3f8892e3d9866676bb6e0e24f83ef64a04b6
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-10-20 16:54:09 +0000

    Parser almost completed
    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Code/pyrat@2629 7821f091-807e-4826-bf48-bc026ed4e502

--- pyrat/fileutils/parameters.py
+++ pyrat/fileutils/parameters.py
@@ -1,4 +1,39 @@
+import datetime as _dt
+
 import pyparsing as _pp
+
+
+def dt_parse(s, l, t):
+    return _dt.date(t['value']['year'], t['value']['month'], t['value']['day'])
+
+
+def int_parse(s, l, t):
+    return int(t[0])
+
+def text_parse(s,l,t):
+    if len(t[0]) == 1:
+        return t[0][0]
+    else:
+        return t
+
+def unit_parse(s, l, t):
+    if len(t['unit']) > 1:
+        return ''.join(t['unit'])
+    elif len(t['unit']) == 1:
+        return t['unit']
+    elif t['unit'] == 's':
+        return t
+
+
+def array_parse(s, l, t):
+    print(t)
+    if len(t[0]) == 1:
+        return t[0]
+    elif 'unit' in t[0]:
+        if t[0]['unit'] == 's':
+            return _dt.timedelta(t[0][0])
+    else:
+        return t
 
 
 class ParameterFile:
@@ -9,35 +44,41 @@ class ParameterFile:
         # separator of keyword
         self.grammar.keyword_sep = _pp.Literal(':').suppress()
         self.grammar.text_parameter = _pp.Group(_pp.Combine(
-            _pp.restOfLine()))
+            _pp.restOfLine())).setResultsName('value')
         # Definition of numbers
         self.grammar.base_units = _pp.Literal('dB') | _pp.Literal('s') | _pp.Literal('m') | _pp.Literal(
             'Hz') | _pp.Literal('degrees') | _pp.Literal('1')
         self.grammar.repeated_unit = (self.grammar.base_units + _pp.Optional('^' + _pp.Word('-123')))
         self.grammar.unit = _pp.Group(
-            self.grammar.repeated_unit + _pp.Optional(_pp.ZeroOrMore('/' + self.grammar.repeated_unit)))
-        self.grammar.float_re = _pp.Regex('[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?')
-        self.grammar.int = _pp.Word(_pp.nums)
-        self.grammar.number = (self.grammar.float_re | self.grammar.int)
+            self.grammar.repeated_unit + _pp.Optional(_pp.ZeroOrMore('/' + self.grammar.repeated_unit))).setResultsName(
+            'unit').setParseAction(unit_parse)
+        self.grammar.float_re = _pp.Regex('[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?').setParseAction(
+            lambda s, l, t: float(t[0]))
+        self.grammar.int = _pp.Word(_pp.nums).setParseAction(int_parse)
+        self.grammar.number = (self.grammar.float_re | self.grammar.int).setResultsName('number')
         # Definition of array:
         array = _pp.Forward()
         array << (
-        (self.grammar.number + array + self.grammar.unit) | (self.grammar.number + _pp.Optional(self.grammar.unit)))
+            (self.grammar.number + array + _pp.Optional(self.grammar.unit)) | (
+            self.grammar.number + _pp.Optional(self.grammar.unit)))
         self.grammar.array = array
         # Date
-        self.grammar.date = _pp.Literal('date').setResultsName('keyword') + self.grammar.keyword_sep + _pp.Group(
-            _pp.Word(_pp.nums + '.').setResultsName('year') + _pp.Word(_pp.nums + '.').setResultsName(
-                'month') + _pp.Word(_pp.nums + '.').setResultsName('day')).setResultsName('date')
+        self.grammar.date = _pp.Literal('date') + self.grammar.keyword_sep + _pp.Group(
+            _pp.Word(_pp.nums + '.').setResultsName('year').setParseAction(int_parse) + _pp.Word(
+                _pp.nums + '.').setResultsName(
+                'month').setParseAction(int_parse) + _pp.Word(_pp.nums + '.').setResultsName('day').setParseAction(
+                int_parse)).setResultsName('value').setParseAction(dt_parse)
         # Line with general text
-        self.grammar.ascii_kw = (
-        _pp.Literal('title') | _pp.Literal('sensor') | _pp.Literal('image_format') | _pp.Literal(
-            'image_geometry') | _pp.Literal('azimuth_deskew') | _pp.Literal('GPRI_TX_mode') |  _pp.Literal('GPRI_TX_antenna')).setResultsName('keyword')
-        self.grammar.ascii_line = self.grammar.ascii_kw + self.grammar.keyword_sep + self.grammar.text_parameter
+        self.grammar.ascii_kw = _pp.Literal('title') | _pp.Literal('sensor') | _pp.Literal('image_format') | _pp.Literal(
+                'image_geometry') | _pp.Literal('azimuth_deskew') | _pp.Literal('GPRI_TX_mode') | _pp.Literal(
+                'GPRI_TX_antenna')
         # normal keyword
         self.grammar.normal_kw = ~_pp.Literal('date') + ~self.grammar.ascii_kw + _pp.Word(_pp.alphanums + '_')
-        self.grammar.normal_line = self.grammar.normal_kw.setResultsName(
-            'keyword') + self.grammar.keyword_sep + _pp.Group(self.grammar.array)
-        self.grammar.line = (self.grammar.normal_line | self.grammar.ascii_line | self.grammar.date) + EOL
+        self.grammar.normal_line = self.grammar.normal_kw + self.grammar.keyword_sep + _pp.Group(self.grammar.array).setResultsName(
+            'value').setParseAction(array_parse)
+        self.grammar.ascii_line = self.grammar.ascii_kw + self.grammar.keyword_sep + self.grammar.text_parameter.setResultsName(
+            'value').setParseAction(text_parse)
+        self.grammar.line = (_pp.Dict(_pp.Group(self.grammar.ascii_line)) | _pp.Dict(_pp.Group(self.grammar.normal_line)) |  _pp.Dict(_pp.Group(self.grammar.date))) + EOL
         self.grammar.param_grammar = _pp.OneOrMore(self.grammar.line)
 
     def to_file(self, outfile):
