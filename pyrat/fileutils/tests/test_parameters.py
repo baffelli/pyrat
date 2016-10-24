3eabc10e339ee4218ac516beb397243868105714
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-10-24 07:36:12 +0000

    Added unittests for parameter files
    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Code/pyrat@2639 7821f091-807e-4826-bf48-bc026ed4e502

---
+++ pyrat/fileutils/tests/test_parameters.py
@@ -0,0 +1,31 @@
+import unittest
+from pyrat.fileutils.parameters import ParameterFile
+
+class TestParameters(unittest.TestCase):
+
+    def setUp(self):
+        self.dummy_val = 5
+        self.params = ParameterFile('./test_params.par')
+
+
+    def testDotAccess(self):
+        self.params.range_samples
+
+    def testItemAccess(self):
+        self.params['range_samples']
+
+
+    def testAccessEquality(self):
+        self.assertEqual(self.params['range_samples'], self.params.range_samples)
+
+
+    def testDotSet(self):
+        self.params.range_samples = self.dummy_val
+
+    def testItemSet(self):
+        self.params['range_samples'] = self.dummy_val
+
+    def testSetEquality(self):
+        self.params.range_samples = self.dummy_val
+        self.params['range_samples'] = self.dummy_val
+        self.assertEqual(self.params.range_samples, self.params['range_samples'])
