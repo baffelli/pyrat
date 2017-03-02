3f2cdbda869f655b62e6bbd473a43075b59f39f1
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-02 16:49:01 +0100

    Added package data for tests to the setup.py script

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 import setuptools
 from setuptools import setup
 
-# all_rules = _glob.glob(_os.path.dirname(__file__) + '/rules/*.snake')
-# rules = {_os.path.splitext(_os.path.basename(rule_path))[0]: rule_path
-#          for rule_path in all_rules}
-
-# print(setuptools.find_packages(exclude=['*tests*']))
-
 # Find all scripts
 wrappers = setuptools.findall('rules/wrappers/*/*.py')
-print(wrappers)
-
 setup(name='pyrat',
       version='0.5',
       description='Python radar tools',
@@ -21,5 +13,7 @@ setup(name='pyrat',
       zip_safe=False,
       install_requires=['numpy', 'matplotlib', 'pyparsing', 'pyfftw', 'gdal', 'pillow'],
       package_data={'rules': ['rules/*.snake'], 'default_slc_params': 'fileutils/default_slc_par.par',
-                    'default_prf': 'fileutils/default_prf.prf'},
+                    'default_prf': 'fileutils/default_prf.prf', 'list_of_slcs':['diff/tests/list_of_slcs.csv'],
+                    },
+      scripts=['gpri_utils/scripts/*.py'],
       include_package_data=True)
