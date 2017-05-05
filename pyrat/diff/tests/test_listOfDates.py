ec222763074ccd2dde287f5582beb27b7ee26972
baffelli <baffelli@ifu.baug.ethz.ch>
2017-05-05 16:56:30 +0200

    Added old tests to the repo.

--- pyrat/diff/tests/test_listOfDates.py
+++ pyrat/diff/tests/test_listOfDates.py
@@ -4,7 +4,6 @@ import glob as glob
 
 from .. import utils
 
-
 class TestListOfDates(TestCase):
     def setUp(self):
         with open('./list_of_slcs.csv') as infile:
@@ -17,11 +16,17 @@ class TestListOfDates(TestCase):
     def test_combinations(self):
         pass
 
+    def testContains(self):
+        a = '20150811_155521' in self.list_of_dates
+        print(a)
+
+
     def test_select_manual_range(self):
         start = '20150803_000000'
         stop = '20150803_180000'
         dates = self.list_of_dates.select_date_range(start, stop)
         print(dates)
+
 
     def test_select_n_dates(self):
         res = self.list_of_dates.select_n_dates(self.dates[0], 1)
